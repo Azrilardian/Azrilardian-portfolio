df847361c46e20044e39d4ef19e4ccbb97acfec7
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2020-12-11 11:29:43 +0800

    Update : menghapus blog

--- js/scrollEffect.js
+++ js/scrollEffect.js
@@ -1,35 +1,31 @@
 //! Scroll Effect
-window.addEventListener("scroll", function() {
-    function scrollEffect(el) {
-        let element = document.querySelectorAll(el);
-        for (let i = 0; i < element.length; i++) {
-            let elementPosition = element[i].getBoundingClientRect().top;
-            let screenPosition = window.innerHeight / 1.4;
-            if (elementPosition < screenPosition) {
-                element[i].classList.add("muncul");
-            }
-        }
-    }
+window.addEventListener("scroll", function () {
+	function scrollEffect(el) {
+		let element = document.querySelectorAll(el);
+		for (let i = 0; i < element.length; i++) {
+			let elementPosition = element[i].getBoundingClientRect().top;
+			let screenPosition = window.innerHeight / 1.4;
+			if (elementPosition < screenPosition) element[i].classList.add("muncul");
+		}
+	}
 
-    function scrollEffectEach(el) {
-        let element = document.querySelectorAll(el);
-        let jumlah = element.length;
-        let mulai = 0;
-        let elementPosition = element[mulai].getBoundingClientRect().top;
-        let screenPosition = window.innerHeight / 1.4;
-        if (elementPosition < screenPosition) {
-            const interval = setInterval(function() {
-                element[mulai].classList.add("muncul");
-                mulai++;
-                if (mulai == jumlah) {
-                    clearInterval(interval);
-                }
-            }, 350);
-        }
-    }
-    scrollEffectEach(".portofolio-parallax");
-    scrollEffect(".scrollEffect");
-    scrollEffect(".team-parallax");
-    scrollEffect(".progress-parallax");
-    scrollEffectEach(".service-parallax");
+	function scrollEffectEach(el) {
+		let element = document.querySelectorAll(el);
+		let jumlah = element.length;
+		let mulai = 0;
+		let elementPosition = element[mulai].getBoundingClientRect().top;
+		let screenPosition = window.innerHeight / 1.4;
+		if (elementPosition < screenPosition) {
+			const interval = setInterval(function () {
+				element[mulai].classList.add("muncul");
+				mulai++;
+				if (mulai == jumlah) clearInterval(interval);
+			}, 350);
+		}
+	}
+	scrollEffectEach(".portofolio-parallax");
+	scrollEffect(".scrollEffect");
+	scrollEffect(".team-parallax");
+	scrollEffect(".progress-parallax");
+	scrollEffectEach(".service-parallax");
 });
