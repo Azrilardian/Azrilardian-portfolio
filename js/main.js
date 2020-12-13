7072cadd119c178403087395c58dd307d28291fb
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2020-12-14 00:52:43 +0800

    Update : memperbaiki overflow pada navbar saat ukuran mobile, mengubah ukuran font pada tag p di ukuran mobile, memperbaiki ukuran loading img pada semua ukuran

--- js/main.js
+++ js/main.js
@@ -21,68 +21,29 @@ $(".page-scroll").on("click", function (e) {
 	});
 });
 
-//! Navigation
 function navigation() {
 	const navContainer = document.querySelector("header nav");
 	const burgerDiv = document.querySelectorAll(".burger div");
 	const ul = document.querySelector("header nav ul");
 	const li = document.querySelectorAll("header nav ul li");
 
-	document.addEventListener("click", function (e) {
-		if (e.target.classList.contains("burger") || e.target.classList.contains("line-one") || e.target.classList.contains("line-two") || e.target.classList.contains("line-three")) {
-			let burger;
-			if (e.target.classList.contains("burger")) burger = e.target;
-			if (e.target.classList.contains("line-one") || e.target.classList.contains("line-two") || e.target.classList.contains("line-three")) burger = e.target.parentElement;
-			ul.classList.toggle("nav-active");
-			li.forEach(function (links, index) {
-				if (links.style.animation) links.style.animation = "";
-				else {
-					links.style.animation = "liFadeIn 0.2s ease forwards " + (index / 7 + 0.3) + "s";
-				}
-			});
-			burger.classList.toggle("toggle");
-			burgerDiv.forEach((e) => e.classList.toggle("active"));
-		} else {
-			const burger = document.querySelector(".burger");
-			burger.classList.remove("toggle");
-			ul.classList.remove("nav-active");
-			li.forEach(function (links, index) {
-				if (links.style.animation) links.style.animation = "";
-				else {
-					links.style.animation = "liFadeIn 0.2s ease forwards " + (index / 7 + 0.3) + "s";
-				}
-			});
-			burgerDiv.forEach((e) => e.classList.remove("active"));
-		}
+	let burger = document.querySelector(".burger");
+	burger.addEventListener("click", function () {
+		ul.classList.toggle("nav-active");
+		this.classList.toggle("toggle");
+		li.forEach((links, index) => (links.style.animation ? (links.style.animation = "") : (links.style.animation = "liFadeIn 0.2s ease forwards " + (index / 7 + 0.3) + "s")));
+		burgerDiv.forEach((e) => e.classList.toggle("active"));
 	});
 
-	window.addEventListener("scroll", function () {
-		if (pageYOffset >= 100) return navContainer.classList.add("black");
-		navContainer.classList.remove("black");
-	});
+	window.addEventListener("scroll", () => (pageYOffset >= 100 ? navContainer.classList.add("black") : navContainer.classList.remove("black")));
 }
 navigation();
 
-//! Button To Top
 function buttonToTop() {
 	const button = document.getElementById("button-to-top");
-	window.addEventListener("scroll", function () {
-		if (pageYOffset > 800) return button.classList.add("show");
-		button.classList.remove("show");
-	});
+	window.addEventListener("scroll", () => (pageYOffset > 800 ? button.classList.add("show") : button.classList.remove("show")));
 }
 buttonToTop();
-
-const certficateFocus = () => {
-	const certificates = document.querySelectorAll(".certificate-parallax .img-container");
-	certificates.forEach((certificate) => {
-		certificate.addEventListener("click", function (e) {
-			certificates.forEach((el) => el.classList.remove("click"));
-			this.classList.add("click");
-		});
-	});
-};
-certficateFocus();
 
 function loading() {
 	const containerImgLoading = document.querySelector(".img-loading");
@@ -97,8 +58,21 @@ function loading() {
 
 	window.addEventListener("load", () => {
 		containerImgLoading.classList.add("loaded");
-		setTimeout(() => containerImgLoading.remove(), 2000);
-		setTimeout(() => allSection.map((e) => e.classList.remove("loading")), 2000);
+		setTimeout(() => {
+			containerImgLoading.remove();
+			allSection.map((e) => e.classList.remove("loading"));
+		}, 1100);
 	});
 }
 loading();
+
+// const certficateFocus = () => {
+// 	const certificates = document.querySelectorAll(".certificate-parallax .img-container");
+// 	certificates.forEach((certificate) => {
+// 		certificate.addEventListener("click", function (e) {
+// 			certificates.forEach((el) => el.classList.remove("click"));
+// 			this.classList.add("click");
+// 		});
+// 	});
+// };
+// certficateFocus();
