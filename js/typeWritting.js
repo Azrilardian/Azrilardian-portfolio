0a523f19e6b11d49e8a9e931a9d146e95d80cb84
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2020-12-15 23:36:13 +0800

    Update : memperbaiki overflow pada saat halaman dibuka, memperbaki typewritting pada jumbotron

--- js/typeWritting.js
+++ js/typeWritting.js
@@ -1,57 +1,57 @@
 //! TypedWritting Effect
-const typeWritter = function(txtElement, words) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.wait = parseInt(wait, 10);
-    this.txt = "";
-    this.wordIndex = 0;
-    this.isDeleting = false;
-    this.type();
+const typeWritter = function (txtElement, words, wait) {
+	this.txtElement = txtElement;
+	this.words = words;
+	this.wait = parseInt(wait, 10);
+	this.txt = "";
+	this.wordIndex = 0;
+	this.isDeleting = false;
+	this.type();
 };
 
 //? Type Method
-typeWritter.prototype.type = function() {
-    //? Current Index of Words
-    const current = this.wordIndex % this.words.length;
-    //? Get full text in Current
-    const fullText = this.words[current];
+typeWritter.prototype.type = function () {
+	//? Current Index of Words
+	const current = this.wordIndex % this.words.length;
+	//? Get full text in Current
+	const fullText = this.words[current];
 
-    //? Check Deleting
-    if (this.isDeleting) {
-        //? Remove char
-        this.txt = fullText.substring(0, this.txt.length - 1);
-    } else {
-        //? Add char
-        this.txt = fullText.substring(0, this.txt.length + 1);
-    }
+	//? Check Deleting
+	if (this.isDeleting) this.txt = fullText.substring(0, this.txt.length - 1);
+	// Remove char
+	else {
+		//? Add char
+		this.txt = fullText.substring(0, this.txt.length + 1);
+	}
 
-    //? Make TypeSpeed
-    let typeSpeed = 100;
-    if (this.isDeleting) {
-        typeSpeed /= 5;
-    }
+	//? Make TypeSpeed
+	let typeSpeed = 100;
+	if (this.isDeleting) typeSpeed /= 5;
+	if (!this.isDeleting && this.txt === fullText) {
+		typeSpeed = this.wait;
+		this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === "") {
+		this.isDeleting = false;
+		this.wordIndex++;
+		typeSpeed = 100;
+	}
 
-    if (!this.isDeleting && this.txt === fullText) {
-        typeSpeed = this.wait;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
-        this.isDeleting = false;
-        this.wordIndex++;
-        typeSpeed = 100;
-    }
-
-    //? Insert Text In To Element
-    this.txtElement.innerHTML = '<span class="txt">' + this.txt + "</span>";
-    setTimeout(() => this.type(), typeSpeed);
+	//? Insert Text In To Element
+	this.txtElement.innerHTML = '<span class="txt">' + this.txt + "</span>";
+	setTimeout(() => this.type(), typeSpeed);
 };
 
 //? Init on DOM Load
-document.addEventListener("DOMContentLoaded", init);
 
-function init() {
-    txtElement = document.querySelector(".text");
-    words = JSON.parse(txtElement.getAttribute("data-words"));
-    wait = txtElement.getAttribute("data-wait");
-    //? Init typeWritter
-    new typeWritter(txtElement, words, wait);
-}
+document.addEventListener("DOMContentLoaded", () => {
+	setTimeout(() => {
+		function init() {
+			const txtElement = document.querySelector(".text");
+			const words = JSON.parse(txtElement.getAttribute("data-words"));
+			const wait = txtElement.getAttribute("data-wait");
+			//? Init typeWritter
+			new typeWritter(txtElement, words, wait);
+		}
+		init();
+	}, 1000);
+});
