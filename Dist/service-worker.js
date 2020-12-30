4070e75a903dcbe8916c264cb80fee1caaac863d
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2020-12-31 00:23:52 +0800

    Update : sinkronisasi cache service worker

--- Dist/service-worker.js
+++ Dist/service-worker.js
@@ -1,8 +1,23 @@
-let CACHE_NAME = "static-cache-v1";
+let CACHE_NAME = "v1";
 let fileToCache = [
-	"./css/style.css",
 	"./bundle.js",
 	"./index.html",
+	"./css/style.css",
+	"./css/bootstrap.min.css",
+	"./css/fontawesome 5.13.0/css/all.min.css",
+	"./css/fontawesome 5.13.0/css/v4-shims.min.css",
+	"./css/fontawesome 5.13.0/webfonts/fa-brands-400.woff2",
+	"./css/fontawesome 5.13.0/webfonts/fa-solid-900.woff2",
+	"./js/Bootstrap-4.5.2/bootstrap.min.js",
+	"./js/Bootstrap-4.5.2/jquery-3.1.1.min.js",
+	"./js/Bootstrap-4.5.2/popper.min.js",
+	"./js/jquery.easing.1.3.js",
+	"./pages/home.html",
+	"./pages/portfolio.html",
+	"./pages/certificate.html",
+	"./fonts/Poppins-Regular.ttf",
+	"./fonts/SourceSansPro-ExtraLight.ttf",
+	"./fonts/SourceSansPro-Regular.ttf",
 	"./img/Certificate/Bitdegree - Introducing Coding for Beginners an HTML and CSS-1.jpg",
 	"./img/Certificate/Bitlabs - Membuat Web dengan Wordpress-1.jpg",
 	"./img/Certificate/Bitlabs - Web Developer-1.jpg",
@@ -59,29 +74,12 @@ let fileToCache = [
 	"./img/undraw_server_down_s4lk.svg",
 	"./img/undraw_signal_searching_bhpc.svg",
 	"./img/ziggy-home.jpg",
-	"./js/Bootstrap-4.5.2/bootstrap.min.js",
-	"./js/Bootstrap-4.5.2/jquery-3.1.1.min.js",
-	"./js/Bootstrap-4.5.2/popper.min.js",
-	"./js/jquery.easing.1.3.js",
-	"./pages/home.html",
-	"./pages/portfolio.html",
-	"./pages/certificate.html",
-	"./fonts/Poppins-Regular.ttf",
-	"./fonts/SourceSansPro-ExtraLight.ttf",
-	"./fonts/SourceSansPro-Regular.ttf",
-	"./favicon.ico",
-	"./manifest.json",
-	"./img/Manifest-Icon/icon-512x512.png",
-	"./css/fontawesome 5.13.0/css/all.min.css",
-	"./css/fontawesome 5.13.0/css/v4-shims.min.css",
-	"./css/fontawesome 5.13.0/webfonts/fa-brands-400.woff2",
-	"./css/fontawesome 5.13.0/webfonts/fa-solid-900.woff2",
 ];
 
 self.addEventListener("install", (e) => e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(fileToCache))));
 
-self.addEventListener("fetch", (e) => e.respondWith(caches.match(e.request).then((res) => (res ? res : fetch(e.request)))));
-
-self.addEventListener("activate", (event) => {
-	event.waitUntil(caches.keys().then((cacheNames) => Promise.all(cacheNames.filter((cacheName) => cacheName != CACHE_NAME).map((cacheName) => caches.delete(cacheName)))));
+self.addEventListener("activate", (e) => {
+	e.waitUntil(caches.keys().then((cacheNames) => Promise.all(cacheNames.filter((cacheName) => cacheName !== CACHE_NAME).map((cacheName) => caches.delete(cacheName)))));
 });
+
+self.addEventListener("fetch", (e) => e.respondWith(caches.match(e.request).then((res) => (res ? res : fetch(e.request)))));
