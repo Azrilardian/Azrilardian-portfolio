c64a7ddac0578ba40575b059b6e736a154d489e0
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2021-01-01 23:46:58 +0800

    Update : fix error menambah file ke cache pada service workers, menambah icon baru dan maskable icon pada manifest.json, memuat asinkronus file style.css, dll

--- App/service-worker.js
+++ App/service-worker.js
@@ -1,7 +1,23 @@
-let CACHE_NAME = "static-cacheV1";
+let CACHE_NAME = "v1";
 let fileToCache = [
-	"./css/bootstrap.min.css",
+	"./bundle.js",
+	"./index.html",
 	"./css/style.css",
+	"./css/bootstrap.min.css",
+	"./css/fontawesome 5.13.0/css/all.min.css",
+	"./css/fontawesome 5.13.0/css/v4-shims.min.css",
+	"./css/fontawesome 5.13.0/webfonts/fa-brands-400.woff2",
+	"./css/fontawesome 5.13.0/webfonts/fa-solid-900.woff2",
+	"./js/Bootstrap-4.5.2/bootstrap.min.js",
+	"./js/Bootstrap-4.5.2/jquery-3.1.1.min.js",
+	"./js/Bootstrap-4.5.2/popper.min.js",
+	"./js/jquery.easing.1.3.js",
+	"./pages/home.html",
+	"./pages/portfolio.html",
+	"./pages/certificate.html",
+	"./fonts/poppins-regular-webfont.woff2",
+	"./fonts/sourcesanspro-extralight-webfont.woff2",
+	"./fonts/sourcesanspro-regular-webfont.woff2",
 	"./img/Certificate/Bitdegree - Introducing Coding for Beginners an HTML and CSS-1.jpg",
 	"./img/Certificate/Bitlabs - Membuat Web dengan Wordpress-1.jpg",
 	"./img/Certificate/Bitlabs - Web Developer-1.jpg",
@@ -29,10 +45,10 @@ let fileToCache = [
 	"./img/imagine-home.jpg",
 	"./img/kalkulator.jpg",
 	"./img/lorem-about.jpg",
-	"./img/lorem-features.png",
+	"./img/lorem-features.jpg",
 	"./img/lorem-home.jpg",
 	"./img/lorem-portfolio.jpg",
-	"./img/lorem-signup.png",
+	"./img/lorem-signup.jpg",
 	"./img/personal-about.jpg",
 	"./img/personal-blog.jpg",
 	"./img/personal-contact.jpg",
@@ -58,36 +74,12 @@ let fileToCache = [
 	"./img/undraw_server_down_s4lk.svg",
 	"./img/undraw_signal_searching_bhpc.svg",
 	"./img/ziggy-home.jpg",
-	"./js/Bootstrap-4.5.2/bootstrap.min.js",
-	"./js/Bootstrap-4.5.2/jquery-3.1.1.min.js",
-	"./js/Bootstrap-4.5.2/popper.min.js",
-	"./js/jquery.easing.1.3.js",
-	"./js/main.js",
-	"./js/scrollEffect.js",
-	"./js/scrollEffectCertificate.js",
-	"./js/scrollEffectPortfolio.js",
-	"./js/scrollEffectSkills.js",
-	"./js/typeWritting.js",
-	"./index.html",
-	"./pages/home.html",
-	"./pages/portfolio.html",
-	"./pages/certificate.html",
-	"./fonts/poppins-regular-webfont.woff2",
-	"./fonts/sourcesanspro-extralight-webfont.woff2",
-	"./fonts/sourcesanspro-regular-webfont.woff2",
-	"./favicon.ico",
-	"./manifest.json",
-	"./img/Manifest-Icon/icon-512x512.png",
-	"./css/fontawesome 5.13.0/css/all.min.css",
-	"./css/fontawesome 5.13.0/css/v4-shims.min.css",
-	"./css/fontawesome 5.13.0/webfonts/fa-brands-400.woff2",
-	"./css/fontawesome 5.13.0/webfonts/fa-solid-900.woff2",
 ];
 
 self.addEventListener("install", (e) => e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(fileToCache))));
 
-self.addEventListener("fetch", (e) => e.respondWith(caches.match(e.request).then((res) => (res ? res : fetch(e.request)))));
-
-self.addEventListener("activate", (event) => {
-	event.waitUntil(caches.keys().then((cacheNames) => Promise.all(cacheNames.filter((cacheName) => cacheName != CACHE_NAME).map((cacheName) => caches.delete(cacheName)))));
+self.addEventListener("activate", (e) => {
+	e.waitUntil(caches.keys().then((cacheNames) => Promise.all(cacheNames.filter((cacheName) => cacheName !== CACHE_NAME).map((cacheName) => caches.delete(cacheName)))));
 });
+
+self.addEventListener("fetch", (e) => e.respondWith(caches.match(e.request).then((res) => (res ? res : fetch(e.request)))));
